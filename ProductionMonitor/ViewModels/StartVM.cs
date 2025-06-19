1b5e1f5d89d18e16475809b77a92ef87be375d0c
Seo-Yegyeong <seoye6618@gmail.com>
2025-06-19 10:27:41 +0900

    feat: Adopt the MVVM pattern

---
+++ ProductionMonitor/ViewModels/StartVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ProductionMonitor.ViewModels
+{
+    public partial class StartVM : ObservableObject
+    {
+        [RelayCommand]
+        public void StartButton()
+        {
+            // Logic to start the production monitor
+            MessageBox.Show("Production Monitor Started");
+        }
+    }
+}
